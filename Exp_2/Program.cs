d88de0c90d28a658c3910cdd01d3cf1c0c227042
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-09-29 22:06:18 +0300

    Написана программ для поиска индекса числа

--- Exp_2/Program.cs
+++ Exp_2/Program.cs
@@ -1,1 +1,16 @@
-﻿int [] array = {11,211,31,14,15,61,71,81,19};
+﻿int [] array = {11,211,31,14,15,61,71,81,19,14};
+
+int n = array.Length;
+int find = 14;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
